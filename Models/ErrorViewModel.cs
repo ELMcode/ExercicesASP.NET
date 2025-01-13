df9a46f387ccc61a3b8dfc66ce8c2c926ed06c1e
amine.elalame <elm.dev.code@gmail.com>
2025-01-13 15:33:50 +0100

    Init MVC architecture

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ExercicesASP.NET.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
